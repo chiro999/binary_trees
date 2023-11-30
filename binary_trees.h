56a093e79dd543e5b61413e9e97433b738f41624
posh <mrposhboo@gmail.com>
2023-12-01 00:24:45 +0100

    tress

--- binary_trees.h
+++ binary_trees.h
@@ -48,4 +48,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 int binary_tree_is_bst(const binary_tree_t *tree);
 size_t depth(const binary_tree_t *tree);
+int remove_type(bst_t *root)
 #endif
